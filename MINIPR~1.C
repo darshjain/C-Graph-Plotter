0a72e5bd94c2c1c6f11b00865ca45200473c609f
Darsh Jain <darshj786@gmail.com>
2020-10-20 14:58:21 +0530

    Added 3D bar chart function

--- MINIPR~1.C
+++ MINIPR~1.C
@@ -6,7 +6,7 @@ int x, y;            //declared x and y a global
 char labels[20][10]; //declared label as global
 int coordinate_adjuster_y(int);
 int coordinate_adjuster_x(int);
-void sorter(int[], int[], int);
+void sorter(int[],int[],int);
 void swapper(int *xp, int *yp)
 {
     int temp = *xp;
@@ -16,6 +16,7 @@ void swapper(int *xp, int *yp)
 void y_plot(int);
 void x_plot(int);
 void bar_chart();
+void bar_3dchart();
 void line_chart();
 void pie_chart();
 int main()
@@ -24,32 +25,80 @@ int main()
     int gd = DETECT, gm;
     initgraph(&gd, &gm, "c:\\turboc3\\bgi");
     setbkcolor(12);
-    rectangle(100, 100, 460, 300);
-    rectangle(110, 110, 450, 290);
-    outtextxy(230, 120, "SELECT GRAPH TYPE");
-    outtextxy(130, 130, "1: LINE GRAPH");
-    outtextxy(130, 140, "2: PIE CHART");
-    outtextxy(130, 150, "3: BAR GRAPH");
+    rectangle(100,100,460,300);
+    rectangle(110,110,450,290);
+    outtextxy(230,120, "SELECT GRAPH TYPE");
+    outtextxy(130,130, "1: LINE GRAPH");
+    outtextxy(130,140, "2: PIE CHART");
+    outtextxy(130,150, "3: BAR GRAPH");
+    outtextxy(130,160, "4: 3D BAR GRAPH");
     scanf("%d", &graph_choice);
     setbkcolor(12);
     switch (graph_choice)
     {
     case 1:
-        line_chart();
-        break;
+	line_chart();
+	break;
     case 2:
-        pie_chart();
-        break;
+	pie_chart();
+	break;
     case 3:
-        bar_chart();
-        break;
+	bar_chart();
+	break;
+    case 4:
+	bar_3dchart();
+	break;
     default:
-        printf("ERROR: Choice Not Found");
-        break;
+	printf("ERROR: Choice Not Found");
+	break;
     }
     getch();
     closegraph();
     return 0;
+}
+void bar_3dchart() //input is labels and numeric
+{
+    int n, i, a[10], b[10], x = 100, factor_y = 20, max_value_y = 0;
+    cleardevice(); //clears screen it involves filling screen with background color
+    printf("Program to Draw a 3D Bar Chart in C Graphics");
+    printf("\n\nEnter Number of values:\t");
+    scanf("%d", &n);
+    printf("\nEnter X values\n");
+    for (i = 0; i < n; i++)
+    { //max values are calculated for coordinate adjuster
+	scanf("%d", &a[i]);
+	if (a[i] > max_value_y)
+	    max_value_y = a[i];
+    }
+    printf("\nEnter Labels \n");
+    for (i = 0; i < n; i++)
+	scanf("%s", labels[i]);
+    factor_y = coordinate_adjuster_y(max_value_y);
+
+    cleardevice(); //clears screen it involves filling screen with background color
+    settextstyle(BOLD_FONT, HORIZ_DIR, 2);
+    outtextxy(220, 10, "3D BAR CHART");
+    settextstyle(2, 1, 0); // font adjustment for labels
+    for (i = 0; i < n; i++)
+    {
+	printf(""); //null
+	setfillstyle(i + 1, i + 1);
+	bar3d(x + 10, 420 - a[i] * factor_y, x + 30, 419,10,1);
+	setcolor(i + 1);
+	outtextxy(x + 10, 425, labels[i]);
+	x += 40;
+    }
+    setcolor(WHITE);
+    //initilizing lines for x and y axis center at(100,420)
+    line(100, 420, 100, 60);  //y axis
+    line(100, 420, 700, 420); //x axis
+    outtextxy(80, 420, "0");
+    y_plot(max_value_y);
+    setcolor(11);
+    rectangle(0,0,639,getmaxy());
+    rectangle(10,10,629,getmaxy()-(10));
+    floodfill(1,1,11);
+    cleardevice(); 
 }
 void bar_chart() //input is labels and numeric
 {
@@ -61,13 +110,13 @@ void bar_chart() //input is labels and numeric
     printf("\nEnter X values\n");
     for (i = 0; i < n; i++)
     { //max values are calculated for coordinate adjuster
-        scanf("%d", &a[i]);
-        if (a[i] > max_value_y)
-            max_value_y = a[i];
+	scanf("%d", &a[i]);
+	if (a[i] > max_value_y)
+	    max_value_y = a[i];
     }
     printf("\nEnter Labels \n");
     for (i = 0; i < n; i++)
-        scanf("%s", labels[i]);
+	scanf("%s", labels[i]);
     factor_y = coordinate_adjuster_y(max_value_y);
 
     cleardevice(); //clears screen it involves filling screen with background color
@@ -76,12 +125,12 @@ void bar_chart() //input is labels and numeric
     settextstyle(2, 1, 0); // font adjustment for labels
     for (i = 0; i < n; i++)
     {
-        printf(""); //null
-        setfillstyle(i + 1, i + 1);
-        bar(x + 10, 420 - a[i] * factor_y, x + 30, 419);
-        setcolor(i + 1);
-        outtextxy(x + 10, 425, labels[i]);
-        x += 40;
+	printf(""); //null
+	setfillstyle(i + 1, i + 1);
+	bar(x + 10, 420 - a[i] * factor_y, x + 30, 419);
+	setcolor(i + 1);
+	outtextxy(x + 10, 425, labels[i]);
+	x += 40;
     }
     setcolor(WHITE);
     //initilizing lines for x and y axis center at(100,420)
@@ -90,9 +139,10 @@ void bar_chart() //input is labels and numeric
     outtextxy(80, 420, "0");
     y_plot(max_value_y);
     setcolor(11);
-    rectangle(0, 0, 639, getmaxy());
-    rectangle(10, 10, 629, getmaxy() - (10));
-    floodfill(1, 1, 11);
+    rectangle(0,0,639,getmaxy());
+    rectangle(10,10,629,getmaxy()-(10));
+    floodfill(1,1,11);
+    cleardevice(); 
 }
 void line_chart() //input is both numeric
 {
@@ -104,16 +154,16 @@ void line_chart() //input is both numeric
     printf("\nEnter X values\n");
     for (i = 0; i < n; i++)
     {
-        scanf("%d", &a[i]);
-        if (a[i] > max_value_x)
-            max_value_x = a[i];
+	scanf("%d", &a[i]);
+	if (a[i] > max_value_x)
+	    max_value_x = a[i];
     }
     printf("\nEnter Y values\n");
     for (i = 0; i < n; i++)
     {
-        scanf("%d", &b[i]);
-        if (b[i] > max_value_y)
-            max_value_y = b[i];
+	scanf("%d", &b[i]);
+	if (b[i] > max_value_y)
+	    max_value_y = b[i];
     }
     //inputs till here for x and y coordinates
     cleardevice();
@@ -123,7 +173,7 @@ void line_chart() //input is both numeric
 
     for (i = 0; i < n - 1; i++)
     {
-        line(100 + a[i] * factor_x - 20, 420 - b[i] * factor_y, 100 + a[i + 1] * factor_x - 20, 420 - b[i + 1] * factor_y);
+	line(100 + a[i] * factor_x - 20, 420 - b[i] * factor_y, 100 + a[i + 1] * factor_x - 20, 420 - b[i + 1] * factor_y);
     }
 
     setcolor(WHITE);
@@ -134,9 +184,9 @@ void line_chart() //input is both numeric
     y_plot(max_value_y);
     x_plot(max_value_x);
     setcolor(11);
-    rectangle(0, 0, 639, getmaxy());
-    rectangle(10, 10, 629, getmaxy() - (10));
-    floodfill(1, 1, 11);
+    rectangle(0,0,639,getmaxy());
+    rectangle(10,10,629,getmaxy()-(10));
+    floodfill(1,1,11);
     getch();
     cleardevice(); //clears screen it involves filling screen with background color
 }
@@ -150,12 +200,12 @@ void pie_chart() //input is labels and numeric-WORKING CODE
     printf("\nEnter X values\n");
     for (i = 0; i < n; i++)
     {
-        scanf("%d", &a[i]);
-        total += a[i];
+	scanf("%d", &a[i]);
+	total += a[i];
     }
     printf("\nEnter Labels \n");
     for (i = 0; i < n; i++)
-        scanf("%s", labels[i]); //input labels
+	scanf("%s", labels[i]); //input labels
 
     cleardevice(); //clears screen it involves filling screen with background color
     settextstyle(BOLD_FONT, HORIZ_DIR, 2);
@@ -166,28 +216,27 @@ void pie_chart() //input is labels and numeric-WORKING CODE
     settextstyle(SANS_SERIF_FONT, HORIZ_DIR, 2);
     for (i = 0; i < n; i++)
     {
-        num = a[i];
-        setfillstyle(SOLID_FILL, i + 2);
-        end_angle = ((float)num * 360 / (float)total);
-        printf("");
-        //printf("%d\n", end_angle);
-        //it prints end_angle as 0 but the very next lines execute perfectly in each loop
-        end_angle += start_angle;
-        pieslice(x_center, y_center, start_angle, end_angle, 120);
-        start_angle = end_angle;
+	num = a[i];
+	setfillstyle(SOLID_FILL, i + 2);
+	end_angle = ((float)num * 360 / (float)total);
+	printf("");
+	//printf("%d\n", end_angle);
+	//it prints end_angle as 0 but the very next lines execute perfectly in each loop
+	end_angle += start_angle;
+	pieslice(x_center, y_center, start_angle, end_angle, 120);
+	start_angle = end_angle;
     }
     for (i = 0; i < n; i++)
     {
-        setcolor(i + 2);
-        outtextxy(table_x, table_y, labels[i]);
-        table_y += 20;
+	setcolor(i + 2);
+	outtextxy(table_x, table_y, labels[i]);
+	table_y += 20;
     }
     setcolor(11);
-    rectangle(0, 0, 639, getmaxy());
-    rectangle(10, 10, 629, getmaxy() - (10));
-    floodfill(1, 1, 11);
+    rectangle(0,0,639,getmaxy());
+    rectangle(10,10,629,getmaxy()-(10));
+    floodfill(1,1,11);
 }
-
 int coordinate_adjuster_y(int maximum) //adjusts the distribution of graph according to inputs
 {
     int factor;
@@ -210,9 +259,9 @@ void y_plot(int max_val) //plot coordinates y axis
     settextstyle(2, 0, 0);
     for (i = 0; i < 10; i++)
     {
-        plot = factor * (i + 1);
-        gcvt(plot, 6, str);
-        outtextxy(75, 420 - 34 * (i + 1), str);
+	plot = factor * (i + 1);
+	gcvt(plot, 6, str);
+	outtextxy(75, 420 - 34 * (i + 1), str);
     }
 }
 void x_plot(int max_val) //plot coordinates y axis
@@ -225,20 +274,20 @@ void x_plot(int max_val) //plot coordinates y axis
     settextstyle(2, 0, 0);
     for (i = 0; i < 10; i++)
     {
-        plot = factor * (i + 1);
-        gcvt(plot, 6, str);
-        outtextxy(75 + 55 * (i + 1), 420, str);
+	plot = factor * (i + 1);
+	gcvt(plot, 6, str);
+	outtextxy(75 + 55 * (i + 1), 420, str);
     }
 }
-void sorter(int x[], int y[], int n) //for line graph
+void sorter(int x[], int y[], int n)//for line graph
 {
     int i, j;
     for (i = 0; i < n - 1; i++)
 
-        for (j = 0; j < n - i - 1; j++)
-            if (x[j] > x[j + 1])
-            {
-                swapper(&x[j], &x[j + 1]);
-                swapper(&y[j], &y[j + 1]);
-            }
+	for (j = 0; j < n - i - 1; j++)
+	    if (x[j] > x[j + 1])
+	    {
+		swapper(&x[j], &x[j + 1]);
+		swapper(&y[j], &y[j + 1]);
+	    }
 }
